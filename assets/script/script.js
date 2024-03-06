e09b94dcd4e48598e9ff9b992e092fe3420bff81
Ghazal Nasiri <9ghazal.nasiri9@gmail.com>
2024-03-06 19:56:14 +0330

    BLACKPINK

---
+++ assets/script/script.js
@@ -0,0 +1,39 @@
+////////// accordion
+const button = document.querySelectorAll("button");
+button.forEach(e => {
+    e.addEventListener("click", () => {
+        button.forEach(element => {
+            if (element != e) {
+                element.classList.remove("active");
+                element.nextElementSibling.style.height = 0;
+            }
+            else {
+                e.classList.toggle("active")
+
+                if (e.classList.contains("active")) {
+                    e.nextElementSibling.style.height = e.nextElementSibling.scrollHeight + "px";
+                }
+                else {
+                    e.nextElementSibling.style.height = 0;
+                }
+            }
+        })
+    })
+});
+///////////// audio
+document.addEventListener('play', e => {
+    let audios = document.getElementsByTagName('audio');
+
+    for (let i = 0; i < audios.length; i++) {
+        if (audios[i] != e.target) {
+            audios[i].pause();
+        }
+    }
+}, true);
+
+///////// loading
+let loading = document.querySelector(".loading");
+window.addEventListener("load", () => {
+    loading.style.display = "none";
+})
+
