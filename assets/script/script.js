8afe347b29c31f75dfe57c457127919ea2acbef8
Ghazal Nasiri <9ghazal.nasiri9@gmail.com>
2024-03-12 09:44:52 +0330

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -21,15 +21,23 @@ button.forEach(e => {
     })
 });
 ///////////// audio
-document.addEventListener('play', e => {
-    let audios = document.getElementsByTagName('audio');
+let audios = document.getElementsByTagName('audio');
 
+document.addEventListener('play', e => {
     for (let i = 0; i < audios.length; i++) {
         if (audios[i] != e.target) {
             audios[i].pause();
         }
     }
 }, true);
+// Audio style on ios
+if (platform.os.family == "iOS") {
+    for (let i = 0; i < audios.length; i++) {
+        audios[i].style = "border: 0;border-radius: 0;box-shadow: none;"
+
+    };
+}
+
 
 ///////// loading
 let loading = document.querySelector(".loading");
@@ -40,12 +48,12 @@ window.addEventListener("load", () => {
 ///////// show image blackpink by touchmove
 let imgBlackpink = document.querySelector(".blackpink");
 let logoBP = document.querySelector(".left");
-logoBP.addEventListener("touchmove" , ()=>{
+logoBP.addEventListener("touchmove", () => {
     imgBlackpink.style.opacity = ".7";
 })
-logoBP.addEventListener("touchend" , ()=>{
+logoBP.addEventListener("touchend", () => {
     imgBlackpink.style.opacity = "0";
 
-    })
+})
 
 
